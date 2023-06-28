27cd72a007d1e4ec02f3a5a66905bdf1fdff72bd
vicleca <victorleocanto@gmail.com>
2023-06-28 22:01:12 +0200

    Add another endpoint

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get('/hello')
 def index():
     return {'message': 'Hello World!'}
+
+@app.get('/blog/all')
+def get_all_blogs():
+	return {'message': 'All blogs provided'}
+
+@app.get('/blog/{id}')
+def get_blog(id: int):
+	return {'message': f'Blog with id {id}'}
