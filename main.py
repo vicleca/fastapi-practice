d44278ff5645f42fcdddbca09c9755b2b2d98bc2
vicleca <victorleocanto@gmail.com>
2023-06-28 22:21:35 +0200

    Add endpoint with predefined values

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -9,6 +10,15 @@ def index():
 @app.get('/blog/all')
 def get_all_blogs():
 	return {'message': 'All blogs provided'}
+
+class BlogType(str, Enum):
+	short = 'short'
+	story = 'story'
+	howto = 'howto'
+
+@app.get('/blog/type/{type}')
+def get_blog_type(type: BlogType):
+	return {'message': f'Blog type {type.value}'}
 
 @app.get('/blog/{id}')
 def get_blog(id: int):
