89737faa0957effbc12cbd2933ea8a482d65dba5
vicleca <victorleocanto@gmail.com>
2023-06-29 22:17:02 +0200

    Add tags

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ app = FastAPI()
 def index():
     return {'message': 'Hello World!'}
 
-@app.get('/blog/all')
+@app.get(
+	'/blog/all',
+	tags = ['blog']
+	)
 def get_all_blogs(page = 1, page_size: Optional[int] = None):
 	return {'message': f'All {page_size} blogs on page {page}'}
 
 
-@app.get('/blog/{id}/comments/{comment_id}')
+@app.get('/blog/{id}/comments/{comment_id}', tags=['blog', 'comment'])
 def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
 	return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
 
@@ -22,11 +25,11 @@ class BlogType(str, Enum):
 	story = 'story'
 	howto = 'howto'
 
-@app.get('/blog/type/{type}')
+@app.get('/blog/type/{type}', tags = ['blog'])
 def get_blog_type(type: BlogType):
 	return {'message': f'Blog type {type.value}'}
 
-@app.get('/blog/{id}', status_code = status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code = status.HTTP_200_OK, tags = ['blog'])
 def get_blog(id: int, response: Response):
 	if id > 5:
 		response.status_code = status.HTTP_404_NOT_FOUND
