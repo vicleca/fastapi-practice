5a8ede7ac8fdbe9968bed0018544cbcd72dc4e22
vicleca <victorleocanto@gmail.com>
2023-06-29 22:27:32 +0200

    Add summary and description

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ def index():
 
 @app.get(
 	'/blog/all',
-	tags = ['blog']
+	tags=['blog'],
+	summary='Retrieve all blogs',
+	description='This api call simulates fetching all blogs'
 	)
 def get_all_blogs(page = 1, page_size: Optional[int] = None):
 	return {'message': f'All {page_size} blogs on page {page}'}
@@ -18,6 +20,14 @@ def get_all_blogs(page = 1, page_size: Optional[int] = None):
 
 @app.get('/blog/{id}/comments/{comment_id}', tags=['blog', 'comment'])
 def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+	"""
+	Simulates retrieving a comment of a blog
+
+	- **id** mandatory path parameter
+	- **comment_id** mandatory path parameter
+	- **valid** optional query parameter
+	- **username** optional query parameter
+	"""
 	return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
 
 class BlogType(str, Enum):
@@ -25,11 +35,11 @@ class BlogType(str, Enum):
 	story = 'story'
 	howto = 'howto'
 
-@app.get('/blog/type/{type}', tags = ['blog'])
+@app.get('/blog/type/{type}', tags=['blog'])
 def get_blog_type(type: BlogType):
 	return {'message': f'Blog type {type.value}'}
 
-@app.get('/blog/{id}', status_code = status.HTTP_200_OK, tags = ['blog'])
+@app.get('/blog/{id}', status_code = status.HTTP_200_OK, tags=['blog'])
 def get_blog(id: int, response: Response):
 	if id > 5:
 		response.status_code = status.HTTP_404_NOT_FOUND
