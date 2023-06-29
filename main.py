f5ad81be189412de4e70a05757c24b8224773816
vicleca <victorleocanto@gmail.com>
2023-06-29 22:29:52 +0200

    Add response description

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ def index():
 	'/blog/all',
 	tags=['blog'],
 	summary='Retrieve all blogs',
-	description='This api call simulates fetching all blogs'
+	description='This api call simulates fetching all blogs',
+	response_description='The list of available blogs'
 	)
 def get_all_blogs(page = 1, page_size: Optional[int] = None):
 	return {'message': f'All {page_size} blogs on page {page}'}
