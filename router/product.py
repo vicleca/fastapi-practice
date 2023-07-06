afbd9ba9a0fddafafb0c9560ce970441a2fdeade
vicleca <victorleocanto@gmail.com>
2023-07-06 19:04:03 +0200

    Add custom responses with documentation

--- router/product.py
+++ router/product.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from fastapi.responses import Response
+from fastapi.responses import Response, HTMLResponse, PlainTextResponse
 
 router = APIRouter(
     prefix='/product',
@@ -12,3 +12,43 @@ products = ['watch', 'camera', 'phone']
 def get_all_products():
     data = " ".join(products)
     return Response(content=data, media_type='text/html')
+
+@router.get('/{id}', responses={
+  200: {
+    "content": {
+      "text/html": {
+        "example": "<div>Product</div>"
+      }
+    },
+    "description": "Returns the HTML for an object"
+  },
+  404: {
+    "content": {
+      "text/plain": {
+        "example": "Product not available"
+      }
+    },
+    "description": "A cleartext error message"
+  }
+})
+def get_product(id: int):
+    if id > len(products):
+        out = "Product not available"
+        return PlainTextResponse(status_code=404, content=out, media_type="text/plain")
+    else:
+        product = products[id]
+        out = f"""
+        <head>
+            <style>
+            .product {{
+                width: 500px;
+                height: 30px;
+                border: 2px inset green;
+                background-color: lightblue;
+                text-align: center;
+            }}
+            </style>
+        </head>
+        <div class="product">{product}</div>
+        """
+        return HTMLResponse(content=out, media_type="text/html")
