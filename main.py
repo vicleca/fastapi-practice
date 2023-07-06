b799ebcf427982f78ea8b6278ee28048fee82bab
vicleca <victorleocanto@gmail.com>
2023-07-06 18:39:30 +0200

    Add custom response

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from router import blog_get
 from router import blog_post
 from router import user
 from router import article
+from router import product
 from db import models
 from db.database import engine
 from fastapi import Request, HTTPException
@@ -12,6 +13,7 @@ from fastapi.responses import JSONResponse, PlainTextResponse
 app = FastAPI()
 app.include_router(user.router)
 app.include_router(article.router)
+app.include_router(product.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 
