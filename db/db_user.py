279ace569beaaf551be636485b58cabbd07ee0ae
vicleca <victorleocanto@gmail.com>
2023-07-09 20:40:27 +0200

    Get current user and secure endpoints

--- db/db_user.py
+++ db/db_user.py
@@ -25,7 +25,15 @@ def get_user(db: Session, id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'User with id {id} not found')
 
-    return
+    return user
+
+def get_user_by_username(db: Session, username: str):
+    user = db.query(DbUser).filter(DbUser.username == username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'User with username {username} not found')
+
+    return user
 
 def update_user(db: Session, id: int, request: UserBase):
     user = db.query(DbUser).filter(DbUser.id == id)
--- router/article.py
+++ router/article.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from db.database import get_db
 from db import db_article
-from auth.oauth2 import oauth2_scheme
+from auth.oauth2 import get_current_user
 
 router = APIRouter(
     prefix='/article',
@@ -13,10 +13,13 @@ router = APIRouter(
 
 # Create article
 @router.post('/', response_model=ArticleDisplay)
-def create_article(request: ArticleBase, db: Session = Depends(get_db)):
+def create_article(request: ArticleBase, db: Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)):
     return db_article.create_article(db, request)
 
 # Get specific article
 @router.get('/{id}') #, response_model=ArticleDisplay)
-def get_article(id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
-    return db_article.get_article(db, id)
+def get_article(id: int, db: Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)):
+    return {
+        'data': db_article.get_article(db, id),
+        'current_user': current_user
+    }
