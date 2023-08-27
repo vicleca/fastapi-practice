9298b3d74f635721f702089a1f0e182e80525117
vicleca <victorleocanto@gmail.com>
2023-08-27 18:26:51 +0200

    Make files statically available

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from db.database import engine
 from fastapi import Request, HTTPException
 from fastapi.responses import JSONResponse, PlainTextResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.include_router(authentication.router)
@@ -45,3 +46,5 @@ app.add_middleware(
 	allow_methods = ["*"],
 	allow_headers = ["*"]
 )
+
+app.mount('/files', StaticFiles(directory="files"), name='files')
