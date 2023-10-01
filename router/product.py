b3d94823b4d36d86f139c5f4660c90f34412aa02
vicleca <victorleocanto@gmail.com>
2023-10-01 18:50:19 +0200

    Add custom log

--- router/product.py
+++ router/product.py
@@ -1,6 +1,7 @@
 from typing import Optional, List
 from fastapi import APIRouter, Header, Cookie, Form
 from fastapi.responses import Response, HTMLResponse, PlainTextResponse
+from custom_log import log
 
 router = APIRouter(
     prefix='/product',
@@ -16,6 +17,7 @@ def create_product(name: str = Form(...)):
 
 @router.get('/all')
 def get_all_products():
+    log("MyAPI", "Call to get all products")
     data = " ".join(products)
     response = Response(content=data, media_type='text/html')
     response.set_cookie(key='test_cookie', value='test_cookie_value')
